8d531bfa003e8cc51fae60a1c35c63c0d7bab511
WebDeveloper-Omar <omorhawlader.dev@gmail.com>
2024-03-13 13:21:07 +0600

    file orgranizing...runing...

---
+++ config/site-map.ts
@@ -0,0 +1,19 @@
+
+
+export const siteConfig = {
+    name: "Omars Blog",
+
+    description:
+    "This is an example Blog App Generated next js ^14+",
+
+    url:"http://localhost:3000",
+
+    links:{
+        github: ""
+    }
+
+
+    
+    
+    
+}
